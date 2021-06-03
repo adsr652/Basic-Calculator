fc1cf13eb6172bcde9989941a97cea7b7318e684
adsr652 <69640573+adsr652@users.noreply.github.com>
2021-06-03 19:43:19 +0530

    Add files via upload

---
+++ Calculation.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter a no."))
+num2 = int(input("Enter 2nd no."))
+cal = int("click 1 for addition, click 2 for minus, click 3 for prod, click 4 div ")
+if(cal==1):
+    print("Addition {num1} and {num2} gives: {num1+num2}"
+if(cal==2):
+    print("Addition {num1} and {num2} gives: {num1-num2}"
+if(cal==3):
+    print("Addition {num1} and {num2} gives: {num1*num2}"
+if(cal==4):
+    print("Addition {num1} and {num2} gives: {num1%num2}"
+else:
+    print("invalid input")
