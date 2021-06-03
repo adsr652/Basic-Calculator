403c328abf7a33dbc33e827a0b996ef17ba80385
adsr652 <69640573+adsr652@users.noreply.github.com>
2021-06-03 23:44:02 +0530

    Update Calculation.py

--- Calculation.py
+++ Calculation.py
@@ -1,13 +1,13 @@
 num1 = int(input("Enter a no."))
 num2 = int(input("Enter 2nd no."))
-cal = int("click 1 for addition, click 2 for minus, click 3 for prod, click 4 div ")
+cal = int(input("click 1 for addition, click 2 for minus, click 3 for prod, click 4 div "))
 if(cal==1):
-    print("Addition {num1} and {num2} gives: {num1+num2}"
-if(cal==2):
-    print("Addition {num1} and {num2} gives: {num1-num2}"
-if(cal==3):
-    print("Addition {num1} and {num2} gives: {num1*num2}"
-if(cal==4):
-    print("Addition {num1} and {num2} gives: {num1%num2}"
+    print("Addition {num1} and {num2} gives: ",(num1+num2))
+elif(cal==2):
+    print("Minus {num1} and {num2} gives: ",(num1-num2))
+elif(cal==3):
+    print("Prod {num1} and {num2} gives: ",(num1*num2))
+elif(cal==4):
+    print("Division {num1} and {num2} gives: ",(num1%num2))
 else:
     print("invalid input")
